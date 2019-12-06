c13012d2eae1cc72f41eda2825fbf971cae9c2b4
unknown <aminuisrael2@gmail.com>
2019-12-06 13:52:41 -0800

    removed variable

--- Deploycrime/settings.py
+++ Deploycrime/settings.py
@@ -128,6 +128,4 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
